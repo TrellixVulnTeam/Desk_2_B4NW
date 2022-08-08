c36c3ab02fcba7765770391035f7002e4fa930c8
brb45 <sunusd@yahoo.com>
2022-08-08 10:33:38 -0700

    8/8

--- log_test.py
+++ log_test.py
@@ -1,15 +1,41 @@
 import psutil
 
-print(psutil.cpu_times())
-print(psutil.cpu_stats())
+# print(psutil.cpu_times())
+# print(psutil.cpu_stats())
 
 
-import re, os
+
+for _ in range(3):
+    cap_possessed_by_person = input().strip().split()
+    print((cap_possessed_by_person))
+    cap_possessed_by_person = map(int, input().strip().split())
+    print(list(cap_possessed_by_person))
+
+# 3
+# ['3']
+# 3
+# [3]
+# 2 10 100
+# ['2', '10', '100']
+# 2 10 100
+# [2, 10, 100]
+# 5 23
+# ['5', '23']
+# 5 23
+# [5, 23]
 
 
-print("\"")
-print(r"\"")
-print('"')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
